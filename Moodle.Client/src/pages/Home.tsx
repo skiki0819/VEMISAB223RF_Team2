f08e2137188ea2317156b7abe272ad45cdf90f6a
Ac1dD4v3 <goczi2002@gmail.com>
2024-04-02 20:26:26 +0200

    api fetch teszt

--- Moodle.Client/src/pages/Home.tsx
+++ Moodle.Client/src/pages/Home.tsx
@@ -1,3 +1,46 @@
-export const Home = () => {
-    return <h1>Home Page</h1>
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+interface Course {
+    id: number;
+    name: string;
+    code: string;
+    credit: number;
 }
+
+export const Home = () => {
+    const [courses, setCourses] = useState<Course[]>([]);
+    const [pageNumber, setPageNumber] = useState(1);
+    const [pageSize, setPageSize] = useState(10);
+
+    useEffect(() => {
+        const fetchCourses = async () => {
+            try {
+                const response = await axios.get('http://localhost:5000/api/Course/courses?pageNumber=${pageNumber}&pageSize=${pageSize}');
+                console.log('API válasz:', response.data);
+                setCourses(response.data);
+            } catch (error) {
+                console.error('Error fetching courses:', error);
+            }
+        };        
+
+        fetchCourses();
+    }, [pageNumber, pageSize]);
+
+    return (
+        <div>
+            <h1>Courses</h1>
+            <ul>
+                {courses.map(course => (
+                    <li key={course.id}>
+                        <h2>{course.name}</h2>
+                        <p>Code: {course.code}</p>
+                        <p>Credit: {course.credit}</p>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default Home;
