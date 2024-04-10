6aac087d36abcf002845b20d30852a58446deec1
szebasztianalbert <szebi04081996@gmail.com>
2024-04-10 22:10:13 +0200

    Select updated

--- Moodle.Client/src/pages/Home.tsx
+++ Moodle.Client/src/pages/Home.tsx
@@ -35,15 +35,24 @@ export const Home = () => {
   const [errorMessage, setErrorMessage] = useState("");
   const [selectedCourseId, setSelectedCourseId] = useState<number | null>(null);
   const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [selectedDegreeId, setSelectedDegreeId] = useState<number>(0);
 
   useEffect(() => {
     const fetchCourses = async () => {
       try {
-        const response = await axios.get(`http://localhost:5191/api/Course`);
-        if (response.data && response.data.success) {
-          setCourses(response.data.data);
-          setFilteredCourses(response.data.data);
-          console.log(response.data);
+        const coursesResponse = await axios.get(
+          `http://localhost:5191/api/Course`
+        );
+        if (coursesResponse.data && coursesResponse.data.success) {
+          setCourses(coursesResponse.data.data);
+          setFilteredCourses(coursesResponse.data.data);
+        }
+        const degreesResponse = await axios.get(
+          `http://localhost:5191/api/Degree/GetAll`
+        );
+        if (degreesResponse.data && degreesResponse.data.success) {
+          setDegrees(degreesResponse.data.data);
+          console.log(degreesResponse.data.data);
         }
       } catch (error) {
         console.error("Error fetching courses:", error);
@@ -132,9 +141,10 @@ export const Home = () => {
       const response = await axios.get(
         `http://localhost:5191/api/Course/GetCoursesByDegree/${degreeId}`
       );
-      if (response.data && response.data.success) {
+      if (response.data.data && response.data.success) {
         setFilteredCourses(response.data.data);
         console.log(response.data.data);
+      } else {
       }
     } catch (error) {
       console.error("Error enrolling:", error);
@@ -222,209 +232,3 @@ export const Home = () => {
     </div>
   );
 };
-
-export default Home;
-
-import { useEffect, useState } from "react";
-import { ChangeEvent } from "react";
-import axios from "axios";
-import { Navbar } from "../components/Navbar";
-import "../styles/Home.css";
-
-interface Course {
-  id: number;
-  name: string;
-  code: string;
-  credit: number;
-}
-
-interface Degree {
-  id: number;
-  name: string;
-}
-
-interface Student {
-  name: string;
-  degree: Degree;
-}
-
-export const Home = () => {
-  const [courses, setCourses] = useState<Course[]>([]);
-  const [degrees, setDegrees] = useState<Degree[]>([]);
-  const [filteredCourses, setFilteredCourses] = useState<Course[]>([]);
-  const [courseNameFilter, setCourseNameFilter] = useState<string>("");
-  const [degreeIdFilter, setDegreeIdFilter] = useState<number | null>(null);
-  const [showStudents, setShowStudents] = useState(false);
-  const [selectedDegreeId, setSelectedDegreeId] = useState<number>(0);
-  const [students, setStudents] = useState([]);
-  const [errorMessage, setErrorMessage] = useState("");
-
-  useEffect(() => {
-    const fetchCourses = async () => {
-      try {
-        const coursesResponse = await axios.get(
-          `http://localhost:5191/api/Course`
-        );
-        if (coursesResponse.data && coursesResponse.data.success) {
-          setCourses(coursesResponse.data.data);
-          setFilteredCourses(coursesResponse.data.data);
-        }
-        const degreesResponse = await axios.get(
-          `http://localhost:5191/api/Degree/GetAll`
-        );
-        if (degreesResponse.data && degreesResponse.data.success) {
-          setDegrees(degreesResponse.data.data);
-          console.log(degreesResponse.data.data);
-        }
-      } catch (error) {
-        console.error("Error fetching courses:", error);
-      }
-    };
-
-    fetchCourses();
-  }, []);
-
-  const AddCourseToUSer = async (courseId: number) => {
-    const userId = localStorage.getItem("userId");
-    if (!userId) {
-      setErrorMessage("Nincs bejelentkezve felhasználó.");
-      return;
-    }
-
-    try {
-      const response = await axios.post(
-        "http://localhost:5191/api/User/AddCourseToUser",
-        {
-          userId: userId,
-          courseId: courseId,
-        }
-      );
-
-      alert(response.data.message); // Felugró ablakban megjelenítjük a response üzenetét
-    } catch (error) {
-      console.error("Error enrolling:", error);
-      setErrorMessage("Hiba történt a felvétel során.");
-    }
-  };
-
-  const handleChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = event.target;
-
-    if (name === "courseNameFilter") {
-      setCourseNameFilter(value);
-      const filtered = courses.filter((course) =>
-        course.name.toLowerCase().includes(value.toLowerCase())
-      );
-      setFilteredCourses(filtered);
-    } else if (name === "degreeIdFilter") {
-      setDegreeIdFilter(Number(value));
-      const filtered = courses.filter((course) =>
-        course.name.toLowerCase().includes(value.toLowerCase())
-      );
-      setFilteredCourses(filtered);
-
-      try {
-        const response = await axios.get(
-          `http://localhost:5191/api/Course/GetCoursesByDegree/${value}`
-        );
-        if (response.data && response.data.success) {
-          setFilteredCourses(response.data.data);
-          console.log(response.data.data);
-        }
-      } catch (error) {
-        console.error("Error fetching courses by degree:", error);
-      }
-    }
-  };
-
-  const handleFilterChange = (event: ChangeEvent<HTMLSelectElement>) => {
-    setSelectedDegreeId(Number(event.target.value));
-    fetchFilteredCourses(Number(selectedDegreeId));
-  };
-
-  const fetchFilteredCourses = async (degreeId: number) => {
-    try {
-      const response = await axios.get(
-        `http://localhost:5191/api/Course/GetCoursesByDegree/${degreeId}`
-      );
-      if (response.data && response.data.success) {
-        setFilteredCourses(response.data.data);
-        console.log(response.data.data);
-      }
-    } catch (error) {
-      console.error("Error enrolling:", error);
-    }
-  };
-
-  return (
-    <div>
-      <Navbar />
-      <h1>Courses</h1>
-
-      <div className="course-filter">
-        <label htmlFor="courseNameFilter" style={{ fontWeight: "bold" }}>
-          Filter by Course Name:
-        </label>
-        <input
-          type="text"
-          id="courseNameFilter"
-          className="filter"
-          name="courseNameFilter"
-          value={courseNameFilter}
-          onChange={handleChange}
-          placeholder="Enter course name"
-        />
-      </div>
-
-      <div className="degree-filter">
-        <label htmlFor="degreeIdFilter" style={{ fontWeight: "bold" }}>
-          Filter by Degree ID:
-        </label>
-        <input
-          type="text"
-          id="degreeIdFilter"
-          className="filter2"
-          name="degreeIdFilter"
-          value={degreeIdFilter ?? ""}
-          onChange={handleChange}
-          placeholder="Enter degree ID"
-        />
-      </div>
-
-      <div className="degree-filter-select">
-        <label htmlFor="filter-select">Szűrés:</label>
-        <select
-          id="filter-select"
-          onChange={handleFilterChange}
-          value={selectedDegreeId}
-        >
-          <option value="">Válassz szakot</option>
-          {degrees.map((degree) => (
-            <option key={degree.id} value={degree.id}>
-              {degree.name}
-            </option>
-          ))}
-        </select>
-      </div>
-
-      <div className="course-container">
-        {filteredCourses.map((course) => (
-          <div key={course.id} className="course">
-            <div className="course-name">{course.name}</div>
-            <div>
-              {course.code}, Credits: {course.credit}
-            </div>
-            <div>
-              <span onClick={() => setShowStudents(true)}>Hallgatók</span>
-            </div>
-            <div>
-              <span onClick={() => AddCourseToUSer(course.id)}>Felvétel</span>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
-};
-
-export default Home;
--- Moodle.Client/src/pages/Login.tsx
+++ Moodle.Client/src/pages/Login.tsx
@@ -1,12 +1,12 @@
-import { useNavigate } from 'react-router-dom';
-import { useState } from 'react';
-import axios from 'axios';
-import '../styles/Login.css';
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
+import "../styles/Login.css";
 
 export const Login = () => {
   const navigate = useNavigate();
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const [error, setError] = useState<string | null>(null);
 
   const handleUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -19,43 +19,53 @@ export const Login = () => {
 
   const handleLogin = async () => {
     try {
-      const response = await axios.post('http://localhost:5191/api/Auth/Login', {
-        username: username,
-        password: password
-      });
-  
+      const response = await axios.post(
+        "http://localhost:5191/api/Auth/Login",
+        {
+          username: username,
+          password: password,
+        }
+      );
+
       const userId = response.data.data.id;
-      localStorage.setItem('userId', userId);
-  
+      localStorage.setItem("userId", userId);
+
       console.log(response.data);
-      navigate('/home', { state: { userId: userId } });
+      navigate("/home", { state: { userId: userId } });
     } catch (error) {
-      if (axios.isAxiosError(error) && error.response && error.response.status === 404) {
+      if (
+        axios.isAxiosError(error) &&
+        error.response &&
+        error.response.status === 404
+      ) {
         const errorMessage = error.response.data;
         console.log(errorMessage);
         setError(errorMessage);
       } else {
-        console.error('Hiba történt a bejelentkezés során:', error);
+        console.error("Hiba történt a bejelentkezés során:", error);
       }
     }
   };
-  
-  
+
   return (
-    <div className='container'>
-      <div className='login-form'>
+    <div className="container">
+      <div className="login-form">
         <h1>Login</h1>
         <div>
-          <label>Username:</label><br />
+          <label>Username:</label>
+          <br />
           <input type="text" value={username} onChange={handleUsernameChange} />
         </div>
         <div>
-          <label>Password:</label><br />
-          <input type="password" value={password} onChange={handlePasswordChange} />
+          <label>Password:</label>
+          <br />
+          <input
+            type="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
         </div>
-        {error && (
-          <div className='error-message'>{error}</div>
-        )}
+        {error && <div className="error-message">{error}</div>}
         <button onClick={handleLogin}>Login</button>
       </div>
     </div>
